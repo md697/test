cf9e7158c4b477eca4c1bc81254787dfffd5ea1a
md697 <md697@CIT-PHILLIPS-02.cornell.edu>
2017-09-16 17:14:12 -0400

    white noise, matlab plotting

--- ipcs_main.m
+++ ipcs_main.m
@@ -1,11 +1,12 @@
 % Parameters
 
-Kd = 1;
+Kd = 3;
 L = 1;
 mCart = 1;
 mPend = 1;
 theta0 = 0;
 x0 = 0;
+tf = 200;
 
 A = [-116.592321910037, -32.461985602428;...
     -71.2609775616371, -3.48905510892101];
@@ -15,3 +16,20 @@ C = [-775.927378314427, 9.53466512662173];
 D = -1590.94866382106;
 
 sim ipcs
+
+exes = simout.Data(:,1);
+thetas = simout.Data(:,2);
+times = simout.Time;
+
+figure(1)
+subplot(2,1,1)
+plot(times,exes,'k-','LineWidth',1)
+title('x vs. time')
+xlabel('t (s)')
+ylabel('x (m)')
+
+subplot(2,1,2)
+plot(times,thetas,'k-','LineWidth',1)
+title('\theta vs. time')
+xlabel('t (s)')
+ylabel('\theta (rad)')
